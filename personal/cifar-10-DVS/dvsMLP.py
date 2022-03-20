37cd7a4316272704f85b2ed2f2bfa117ecbfb554
Denis-Cristian Petre <denis-cristian.petre@student.manchester.ac.uk>
2022-03-20 12:52:03 +0000

    Refactor code in the cifar10dvs

--- personal/cifar-10-DVS/dvsMLP.py
+++ personal/cifar-10-DVS/dvsMLP.py
@@ -3,7 +3,7 @@ import slayerSNN as snn
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 from AedatLegacy import LegacyAedatFile
 import os
 from learningstats import learningStats
@@ -16,8 +16,7 @@ from torch.autograd import Variable, Function
 import aedat
 
 
-logging.basicConfig(filename='dvscifar10.log',
-                    encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='dvscifar10.log', level=logging.DEBUG)
 
 
 categories = ["airplane", "automobile", "bird", "cat",
@@ -28,10 +27,13 @@ netParams = snn.params("network.yaml")
 device = torch.device("cuda")
 
 NUM_EPOCHS = 30
-LEARNING_RATE = 0.0002
+LEARNING_RATE = 0.0001
 BATCH_SIZE = 1
 NUM_WORKERS = 0
-SCALE_OVERFIT = 1
+SCALE_OVERFIT = 10
+
+logging_time = 10
+gradient_logging_time = 100
 
 
 def iter_graph(root, callback):
@@ -47,26 +49,38 @@ def iter_graph(root, callback):
                 queue.append(next_fn)
         callback(fn)
 
+
 def register_hooks(var):
     fn_dict = {}
+
     def hook_cb(fn):
         def register_grad(grad_input, grad_output):
             fn_dict[fn] = grad_input
         fn.register_hook(register_grad)
     iter_graph(var.grad_fn, hook_cb)
 
-    def is_bad_grad(grad_output):
+    def is_bad_grad(grad_output, name):
         if grad_output is None:
             return False
+
+        logging.debug(f"{name}")
+        logging.debug("______________________________")
+        logging.debug(grad_output.shape)
+        logging.debug("______________________________")
+        if str(name) == "SumBackward0":
+            for j in range(10):
+                logging.debug(torch.sum(grad_output[0][j][0][0]))
+        logging.debug(grad_output)
+        logging.debug("______________________________")
         return grad_output.isnan().any() or (grad_output.abs() >= 1e6).any()
 
     def make_dot():
         node_attr = dict(style='filled',
-                        shape='box',
-                        align='left',
-                        fontsize='12',
-                        ranksep='0.1',
-                        height='0.2')
+                         shape='box',
+                         align='left',
+                         fontsize='12',
+                         ranksep='0.1',
+                         height='0.2')
         dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
 
         def size_to_str(size):
@@ -80,9 +94,10 @@ def register_hooks(var):
             else:
                 assert fn in fn_dict, fn
                 fillcolor = 'white'
-                if any(is_bad_grad(gi) for gi in fn_dict[fn]):
+                if any(is_bad_grad(gi, str(type(fn).__name__)) for gi in fn_dict[fn]):
                     fillcolor = 'red'
-                dot.node(str(id(fn)), str(type(fn).__name__), fillcolor=fillcolor)
+                dot.node(str(id(fn)), str(type(fn).__name__),
+                         fillcolor=fillcolor)
             for next_fn, _ in fn.next_functions:
                 if next_fn is not None:
                     next_id = id(getattr(next_fn, 'variable', next_fn))
@@ -122,7 +137,8 @@ class Cifar10DVS(Dataset):
         p = []
         t = []
 
-        desired = torch.empty([10, 1, 1, 1])
+        desired = torch.zeros([10, 1, 1, 1])
+
         desired[category, ...] = 1
 
         sample_index = "dataset4/" + sample_index
@@ -131,11 +147,12 @@ class Cifar10DVS(Dataset):
         for packet in decoder:
             if 'events' in packet:
                 for i in range(len(packet['events'])):
-                    x.append(packet['events'][i][1])
-                    y.append(packet['events'][i][2])
-                    t.append(packet['events'][i][0] / 1000)
-                    p.append(packet['events'][i][3])
-        
+                    if (packet['events'][i][0] / 1000) < self.nTimeBins:
+                        x.append(packet['events'][i][1])
+                        y.append(packet['events'][i][2])
+                        t.append(packet['events'][i][0] / 1000)
+                        p.append(packet['events'][i][3])
+
         decoder = None
 
         x = np.array(x)
@@ -143,7 +160,7 @@ class Cifar10DVS(Dataset):
         p = np.array(p)
         t = np.array(t)
 
-        return snn.io.event(x, y, p, t).toSpikeTensor(torch.empty((2, 128, 128, 1400))), category, desired
+        return snn.io.event(x, y, p, t).toSpikeTensor(torch.empty((2, 128, 128, self.nTimeBins))), category, desired
 
     def __len__(self):
         return len(self.samples)
@@ -176,10 +193,13 @@ def overfit_single_batch():
     dataset_test = Cifar10DVS(netParams['training']['path']['in'], netParams['training']
                               ['path']['test'], netParams['simulation']['Ts'], netParams['simulation']['tSample'])
 
+    dataset_train_valid, dataset_validation, dump_dataset = random_split(
+        dataset_train, [100, 100, 8800])
+
     loaded_train = DataLoader(
-        dataset_train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
-    loaded_test = DataLoader(
-        dataset_test, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+        dataset_train_valid, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
+    loaded_valid = DataLoader(
+        dataset_validation, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
 
     model = Network(netParams).to(device)
     criterion = snn.loss(netParams).to(device)
@@ -187,31 +207,53 @@ def overfit_single_batch():
 
     stats = learningStats()
 
-    # Get single batch
-    (sample, label, desired) = next(iter(loaded_train))
-    sample = sample.to(device)
-    label = label.to(device)
-    desired = desired.to(device)
+    for epoch in range(NUM_EPOCHS):
+        tSt = datetime.now()
+        for i, (sample, label, desired) in enumerate(loaded_train):
+            sample = sample.to(device)
+            output = model(sample)
+            desired = desired.to(device)
 
-    for epoch in range(NUM_EPOCHS * SCALE_OVERFIT):
-        output = model(sample)
+            stats.training.correctSamples += torch.sum(
+                snn.predict.getClass(output) == label).data.item()
+            stats.training.numSamples += len(label)
 
-        for j in range(10):
-            print(
-                f"The number of times neuron {j} fired is {torch.sum(output[0][j][0][0])}")
+            loss = criterion.numSpikes(output, desired)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        loss = criterion.numSpikes(output, desired)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            stats.training.lossSum += loss.cpu().data.item()
 
-        print(loss.item())
+            if i % 2 == 0:
+                stats.print(epoch, i, (datetime.now() - tSt).total_seconds())
+
+        for i, (sample, label, desired) in enumerate(loaded_valid):
+            sample = sample.to(device)
+
+            desired = desired.to(device)
+
+            output = model(sample)
+
+            stats.testing.correctSamples += torch.sum(
+                snn.predict.getClass(output) == label).data.item()
+            stats.testing.numSamples += len(label)
+
+            loss = criterion.numSpikes(output, desired)
+
+            stats.testing.lossSum += loss.cpu().data.item()
+            if i % 2 == 0:
+                stats.print(epoch, i)
+
+        stats.update()
 
 
 def main():
 
-    # with zipfile.ZipFile("dataset.zip", "r") as zip_ref:
-    #     zip_ref.extractall("dataset")
+    with zipfile.ZipFile('dataset4.zip') as zip_file:
+        for member in zip_file.namelist():
+            if not os.path.exists('./dataset/' + member):
+                zip_file.extract(member, './dataset')
 
     dataset_train = Cifar10DVS(netParams['training']['path']['in'], netParams['training']
                                ['path']['train'], netParams['simulation']['Ts'], netParams['simulation']['tSample'])
@@ -227,13 +269,14 @@ def main():
     model = Network(netParams).to(device)
     criterion = snn.loss(netParams).to(device)
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=LEARNING_RATE, weight_decay=0.03)
+        model.parameters(), lr=LEARNING_RATE, weight_decay=0.6)
 
     stats = learningStats()
 
     for i in range(5):
-        sample, label, desired = dataset_train[i*100]
-        snn.io.showTD(snn.io.spikeArrayToEvent(sample.reshape((2,128,128,-1)).cpu().data.numpy()))
+        sample, label, desired = dataset_train[i + 150]
+        snn.io.showTD(snn.io.spikeArrayToEvent(
+            sample.reshape((2, 128, 128, -1)).cpu().data.numpy()))
 
     for epoch in range(NUM_EPOCHS):
         tSt = datetime.now()
@@ -243,7 +286,7 @@ def main():
 
             output = model(sample)
 
-            if i % 1 == 0:
+            if i % logging_time == 0:
 
                 layer1_psp = model.slayer.psp(model.fc1(sample))
                 layer1_spikes = model.slayer.spike(layer1_psp)
@@ -251,9 +294,12 @@ def main():
                 layer2_psp = model.slayer.psp(model.fc2(layer1_spikes))
 
                 for j in range(410):
-                    logging.debug(f"The number of times neuron {j} fired is {torch.sum(layer1_spikes[0][j][0][0])}")
-                    avg = float(torch.sum(layer1_psp[0][j][0][0])) / float(torch.numel(layer1_psp[0][j][0][0]))
-                    logging.debug(f"The average membrane potential for neuron {j} is {avg}")
+                    logging.debug(
+                        f"The number of times neuron {j} fired is {torch.sum(layer1_spikes[0][j][0][0])}")
+                    avg = float(
+                        torch.sum(layer1_psp[0][j][0][0])) / float(torch.numel(layer1_psp[0][j][0][0]))
+                    logging.debug(
+                        f"The average membrane potential for neuron {j} is {avg}")
 
                 for j in range(10):
                     logging.debug(
@@ -272,23 +318,23 @@ def main():
             loss = criterion.numSpikes(output, desired)
             optimizer.zero_grad()
 
-            if i % 50 == 0:
+            if i % gradient_logging_time == 0:
                 get_dot = register_hooks(loss)
                 loss.backward()
                 dot = get_dot()
                 dot.render("gradCifar10DVS", format="png")
             else:
                 loss.backward()
+
             optimizer.step()
 
             stats.training.lossSum += loss.cpu().data.item()
 
-            if i % 2 == 0:
+            if i % 10 == 0:
                 stats.print(epoch, i, (datetime.now() - tSt).total_seconds())
 
         for i, (sample, label, desired) in enumerate(loaded_test):
             sample = sample.to(device)
-            label = label.to(device)
             desired = desired.to(device)
 
             output = model(sample)
